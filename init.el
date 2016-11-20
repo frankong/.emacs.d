bab2bf57f94bb472eef37da2678b9461475e8c98
Frank Ong <frankong@airbears2-10-142-33-123.airbears2.1918.berkeley.edu>
2016-11-20 14:16:08 -0800

    remove custome

--- init.el
+++ init.el
@@ -38,20 +38,3 @@
 
 ;; Load tool related init file
 (load "~/.emacs.d/init_packages.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" default)))
- '(package-selected-packages
-   (quote
-    (smart-mode-line use-package undo-tree smartparens reveal-in-osx-finder nose markdown-mode magit iedit helm-projectile helm-gtags flycheck-tip flycheck-pos-tip cuda-mode company auctex anaconda-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
