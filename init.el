c864fd2e2fd06992f3f252ff6b0862b36151b361
Frank Ong <frank@plaicin.com>
2025-06-19 14:16:53 -0700

    Add claude code

--- init.el
+++ init.el
@@ -44,10 +44,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(swift-mode php-mode phps-mode csv-mode exec-path-from-shell dtrt-indent dockerfile-mode cmake-mode django-mode pytest yaml-mode use-package undo-tree smartparens smart-mode-line reveal-in-osx-finder nose matlab-mode markdown-mode magit iedit helm-projectile helm-gtags flycheck-pos-tip cuda-mode company auctex))
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((claude-code :url "https://github.com/stevemolitor/claude-code.el")))
  '(preview-gs-options
-   '("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4"))
+   '("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4"
+     "-dGraphicsAlphaBits=4"))
  '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- init_packages.el
+++ init_packages.el
@@ -217,3 +217,10 @@
 (use-package php-mode
   :ensure t
   )
+
+
+;; Claude
+(use-package claude-code :ensure t
+  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+  :config (claude-code-mode)
+  :bind-keymap ("C-c c" . claude-code-command-map))
